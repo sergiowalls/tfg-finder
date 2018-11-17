503962eb112b17c2d8e13085467d88de00e135ed
Sergio <sergio.paredes25@gmail.com>
2018-11-17 23:33:27 +0100

    Add proposal form

---
+++ web/public/js/proposalForm.js
@@ -0,0 +1,38 @@
+(() => {
+
+    document.addEventListener('DOMContentLoaded', function () {
+        var elems = document.querySelectorAll('.chips');
+        var instances = M.Chips.init(elems, options);
+    });
+
+    $('.chips').chips();
+
+    $('.chips-placeholder').chips({
+        placeholder: 'Enter a tag',
+        secondaryPlaceholder: '+Tag',
+    });
+
+    let itemsId = 0;
+
+    window.addGoal = function () {
+        let newGoal = $('#goal');
+        let itemId = itemsId++;
+        let newItem = '<li id="' + itemId + '" class="collection-item"><div>' + newGoal.val() +
+            '<a href="#" class="secondary-content"><i class="material-icons waves-effect" onclick="removeGoal(' +
+            itemId + ')">delete</i></a></div></li>';
+        $('#goals').append(newItem);
+        newGoal.val('');
+    };
+
+    window.removeGoal = function (id) {
+        $('#' + id).remove();
+    };
+
+    window.updateAddButton = function () {
+        let disabled;
+        $('#goal').val() ?
+            disabled = false : disabled = true;
+        $('#addButton').attr("disabled", disabled);
+    };
+
+})();
--- web/server.js
+++ web/server.js
@@ -7,7 +7,11 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/html/index.html'))
 });
 
-app.use(express.static('public'))
+app.get('/proposals/create', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/html/proposal_create.html'))
+});
+
+app.use(express.static('public'));
 
 app.listen(3001, () => {
   console.log('Listening to port 3001');
