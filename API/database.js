0ded2d9296264d4e9a8cb2e9bfdabddd90bf93c6
Gonzalo Recio <gonzalo.recio@est.fib.upc.edu>
2018-11-18 02:48:42 +0100

    Refactor Historical proposals

--- API/database.js
+++ API/database.js
@@ -2,10 +2,10 @@ const sqlite3 = require('sqlite3').verbose();
 Database = {};
 
 function getDatabase() {
-	// open the database with Foreign Key Support
-	let db = new sqlite3.Database('./tfg_finder.db');
-	db.get('PRAGMA foreign_keys = ON;');
-	return db;
+    // open the database with Foreign Key Support
+    let db = new sqlite3.Database('./tfg_finder.db');
+    db.get('PRAGMA foreign_keys = ON;');
+    return db;
 }
 
 /*function createInitialTables() {
@@ -26,64 +26,64 @@ function getDatabase() {
 }
 */
 function returnAllRows(sql, callback) {
-	db = getDatabase();
- 
-	db.all(sql, [], (err, rows) => {
-	  if (err) {
-	    throw err;
-	  }
-	  callback(rows);
-	});
-	 
-	// close the database connection
-	db.close();
+    db = getDatabase();
+
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        callback(rows);
+    });
+
+    // close the database connection
+    db.close();
 }
 
-Database.getAllProposals = function(tag, callback) {
-	let sql = `SELECT * FROM proposals`;
-	returnAllRows(sql, (rows)=>{
-		var proposals = rows.map((row) => {
-			return {
-				id:row.id,
-				title:row.title,
-				description:row.description,
-				goals:row.goals.split('|'),
-				keywords:row.keywords.split('|'),
-				state:row.state,
-				proposer:row.proposer,
-                subscriber:row.subscriber,
-                created_at:row.created_at
-			}
-		});
-		if (tag) {
-            proposals = proposals.filter((p) => {return p.keywords.indexOf(tag) > -1});
+Database.getAllProposals = function (tag, callback) {
+    let sql = `SELECT * FROM proposals`;
+    returnAllRows(sql, (rows) => {
+        var proposals = rows.map((row) => {
+            return {
+                id: row.id,
+                title: row.title,
+                description: row.description,
+                goals: row.goals.split('|'),
+                keywords: row.keywords.split('|'),
+                state: row.state,
+                proposer: row.proposer,
+                subscriber: row.subscriber,
+                created_at: row.created_at
+            }
+        });
+        if (tag) {
+            proposals = proposals.filter((p) => {
+                return p.keywords.indexOf(tag) > -1
+            });
         }
         callback(proposals)
-	});
+    });
 };
 
-Database.getAllUsers = function(callback) {
-	let sql = `SELECT * FROM users`;
- 	returnAllRows(sql, callback);
+Database.getAllUsers = function (callback) {
+    let sql = `SELECT * FROM users`;
+    returnAllRows(sql, callback);
 };
 
-Database.getProposal = function(proposal_id, callback, callbackErr) {
+Database.getProposal = function (proposal_id, callback, callbackErr) {
     let db = getDatabase();
     db.get(`SELECT  * FROM proposals WHERE id=${proposal_id};`,
-        function(err, rows) {
+        function (err, rows) {
             if (err) {
                 console.log(err.message);
                 callbackErr();
-            }
-            else{
-            	if (rows) {
-            		let proposal = rows;
-            		proposal.goals  = proposal.goals.split('|');
-            		proposal.keywords = proposal.keywords.split('|');
-            		callback(rows);
+            } else {
+                if (rows) {
+                    let proposal = rows;
+                    proposal.goals = proposal.goals.split('|');
+                    proposal.keywords = proposal.keywords.split('|');
+                    callback(rows);
 
-            	}
-            	else callback({});
+                } else callback({});
             }
         });
     db.close();
@@ -104,15 +104,14 @@ Database.getUserKeywords = function(user_email, callback, callbackErr) {
     db.close();
 };
 
-Database.getProposalHistoric = function(proposal_id, callback, callbackErr) {
+Database.getProposalHistoric = function (proposal_id, callback, callbackErr) {
     let db = getDatabase();
     db.get(`SELECT  * FROM historical_proposals WHERE id_proposal=${proposal_id};`,
-        function(err, rows) {
+        function (err, rows) {
             if (err) {
                 console.log(err.message);
                 callbackErr();
-            }
-            else{
+            } else {
                 if (rows) callback(rows);
                 else callback({});
             }
@@ -120,42 +119,36 @@ Database.getProposalHistoric = function(proposal_id, callback, callbackErr) {
     db.close();
 };
 
-Database.createProposal = function(proposal, callback, callbackErr) {
-	let db = getDatabase();
-	console.log(proposal);
-	console.log(proposal.goals);
-	let fields = [proposal.title, proposal.description, proposal.goals.join('|'), proposal.keywords.join('|'),
-        proposal.state, proposal.proposer, proposal.subscriber, Date.now().toString()];
-    let values = fields.map((field) => '(?)').join(',');
-    console.log(values);
-    console.log(fields);
-	let sql = `INSERT INTO proposals(title, description, goals, keywords, state, proposer, subscriber, created_at) VALUES ('`+ fields.join("','") + "');";
-	console.log(sql);
-	db.run(sql, [],
-		function(err) {
-			if (err) {
-			   console.log(err.message);
-			   callbackErr();
-			}
-			else{
-				callback();
-			}
-  	});
-
-  	db.close();
-};
-
-Database.createHistoricProposal = function(proposal, id_proposal, callback, callbackErr) {
+Database.createProposal = function (proposal, callback, callbackErr) {
     let db = getDatabase();
-    db.run(`INSERT INTO historical_proposals(proposal) VALUES(?)`,
-        [proposal.title, proposal.description, proposal.goals.join('|'), proposal.keywords.join('|'),
-            proposal.state, proposal.proposer, proposal.subscriber, id_proposal, proposal.created_at,],
-        function(err) {
+
+    let fields = [proposal.title, proposal.description, proposal.goals.join('|'), proposal.keywords.join('|'),
+        proposal.state, proposal.proposer, proposal.subscriber, Date.now().toString()];
+    // let values = fields.map((field) => '(?)').join(',');
+
+    let proposal_id = null
+
+    let sql = `INSERT INTO proposals(title, description, goals, keywords, state, proposer, subscriber, created_at) VALUES ('` + fields.join("','") + "');";
+    console.log(sql);
+    db.run(sql, [],
+        function (err, result) {
             if (err) {
                 console.log(err.message);
                 callbackErr();
+            } else {
+                proposal_id = result;
+                // callback();
             }
-            else{
+        });
+    fields = [proposal.title, proposal.description, proposal.goals.join('|'), proposal.keywords.join('|'),
+        proposal.state, proposal.proposer, proposal.subscriber, proposal_id, Date.now().toString()];
+    sql = `INSERT INTO historical_proposals(title, description, goals, keywords, state, proposer, subscriber, id_proposal, created_at) VALUES ('` + fields.join("','") + "');";
+    db.run(sql, [],
+        function (err) {
+            if (err) {
+                console.log(err.message);
+                callbackErr();
+            } else {
                 callback();
             }
         });
@@ -163,38 +156,40 @@ Database.createHistoricProposal = function(proposal, id_proposal, callback, call
     db.close();
 };
 
-Database.modifyProposal = function(proposal, callback, callbackErr) {
-	let db = getDatabase();
-	let old_proposal = null;
-    db.get(`SELECT  * FROM proposals WHERE id=${proposal.id};`,
-        function(err, rows) {
+Database.createHistoricProposal = function (proposal, id_proposal, callback, callbackErr) {
+    let db = getDatabase();
+    db.run(`INSERT INTO historical_proposals(proposal) VALUES(?)`,
+        [proposal.title, proposal.description, proposal.goals.join('|'), proposal.keywords.join('|'),
+            proposal.state, proposal.proposer, proposal.subscriber, id_proposal, proposal.created_at,],
+        function (err) {
             if (err) {
                 console.log(err.message);
                 callbackErr();
-            }
-            else{
-                if (rows) {old_proposal=rows;}
-                else callbackErr();
+            } else {
+                callback();
             }
         });
 
-	db.run(`UPDATE proposals SET title=${proposal.title}, ,  , WHERE id=${proposal.id} `,
-		[proposal.title, proposal.description, proposal.goals.join('|'), proposal.keywords.join('|'),
-			proposal.state, proposal.proposer, proposal.subscriber, Date.now(),],
-		function(err) {
-			if (err) {
-			   console.log(err.message);
-			   callbackErr();
-			}
-			else{
-				callback();
-			}
-  	});
-
-	this.createHistoricProposal(old_proposal, proposal.id);
-  	db.close();
+    db.close();
 };
 
+Database.modifyProposal = function (proposal, callback, callbackErr) {
+    let db = getDatabase();
+
+    db.run(`UPDATE proposals SET title=${proposal.title}, description= , goals=${proposal.goals.join('|')}, keywords=${proposal.keywords.join('|')}, state=${proposal.state}, proposer=${proposal.proposer}, subscriber=${proposal.subscriber}, created_at=${Date.now()} WHERE id=${proposal.id} `,
+        [],
+        function (err, result) {
+            if (err) {
+                console.log(err.message);
+                callbackErr();
+            } else {
+                this.createHistoricProposal(result, proposal.id);
+                callback();
+            }
+        });
+
+    db.close();
+};
 
 
 //createInitialTables();
