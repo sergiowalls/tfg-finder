218bde3e81331dee01f25b58a46f13a9f590b66a
Pau Torrents <pautg.95@gmail.com>
2018-11-18 03:50:39 +0100

    Second prototype

--- web/public/js/proposalDetail.js
+++ web/public/js/proposalDetail.js
@@ -29,7 +29,7 @@
         break;
       case 'modified':
         html = 'en proc&eacute;s'
-        stateClass = 'yellow'
+        stateClass = 'orange'
         break;
       case 'pending':
         html = 'pendent de validaci&oacute;'
