55d92a6b6aaeb83d707b7b82d3d6bf392eb8a2fa
Sergio <sergio.paredes25@gmail.com>
2018-11-18 03:00:49 +0100

    Add on chips add and on delete

--- web/public/js/tags.js
+++ web/public/js/tags.js
@@ -3,7 +3,15 @@
     document.addEventListener('DOMContentLoaded', function () {
         var elems = document.querySelectorAll('.chips');
         M.Chips.init(elems, {
-            onChipSelect: (e, chip) => {console.log(chip.childNodes[0].textContent)}
+            onChipSelect: (e, chip) => {console.log(chip.childNodes[0].textContent)},
+            onChipAdd: (e, chip) => {
+                let chips = M.Chips.getInstance($('.chips')).chipsData.map((x) => x.tag);
+                console.log(chips);
+            },
+            onChipDelete: (e, chip) => {
+                let chips = M.Chips.getInstance($('.chips')).chipsData.map((x) => x.tag);
+                console.log(chips);
+            }
         });
     });
 
