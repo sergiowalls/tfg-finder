fa4691314e2fb9c11d284c20b352dbce75f5eafd
Pau Torrents <pautg.95@gmail.com>
2018-11-18 12:08:24 +0100

    Whatever

--- web/public/js/proposalForm.js
+++ web/public/js/proposalForm.js
@@ -1,5 +1,8 @@
 (() => {
 
+    window.goals = [];
+    window.keywords = [];
+
     document.addEventListener('DOMContentLoaded', function () {
         var elems = document.querySelectorAll('.chips');
         var instances = M.Chips.init(elems, options);
@@ -21,12 +24,19 @@
             '<a href="#" class="secondary-content"><i class="material-icons waves-effect" onclick="removeGoal(' +
             itemId + ')">delete</i></a></div></li>';
         $('#goals').append(newItem);
+        goals.push(newGoal.val());
         newGoal.val('');
         updateAddButton();
     };
 
     window.removeGoal = function (id) {
+        let goal = $('#' + id).val();
         $('#' + id).remove();
+
+        var index = goals.indexOf(goal);
+        if (index !== -1) {
+            goals.splice(index, 1);
+        }
     };
 
     window.updateAddButton = function () {
@@ -35,5 +45,24 @@
             disabled = false : disabled = true;
         $('#addButton').attr("disabled", disabled);
     };
+
+    window.submitForm = function() {
+        const json = {
+          title: $('#title').val(),
+          description: $('#description').val(),
+          goals: window.goals,
+          keywords: window.keywords
+        };
+    
+        $.ajax({
+          type: 'POST',
+          url: 'http://localhost:3000/proposals',
+          contentType: 'application/json',
+          data: json,
+          success: () => {
+            window.location.href = "http://localhost:3001/";
+          }
+        })
+      }
 
 })();
