eb26a04a7d2e2bbd3be010b871a04ca5d6b33fdf
Gonzalo Recio <gonzalo.recio@est.fib.upc.edu>
2018-11-18 10:23:54 +0100

    Get and update user tags

--- web/public/js/tags.js
+++ web/public/js/tags.js
@@ -1,29 +1,42 @@
 (() => {
 
-    document.addEventListener('DOMContentLoaded', function () {
-        var elems = document.querySelectorAll('.chips');
-        M.Chips.init(elems, {
-            onChipSelect: (e, chip) => {console.log(chip.childNodes[0].textContent)},
-            onChipAdd: (e, chip) => {
-                let chips = M.Chips.getInstance($('.chips')).chipsData.map((x) => x.tag);
-                console.log(chips);
-                $.ajax({
-                    url: "http://localhost:3003/users/" + localStorage.getItem('user') + "/keywords",
-                    type: 'PUT',
-                    data: JSON.stringify(chips),
-                    contentType: 'application/json',
-                    success: function(result) {
-                        // Do something with the result
-                    }});
-            },
-            onChipDelete: (e, chip) => {
-                let chips = M.Chips.getInstance($('.chips')).chipsData.map((x) => x.tag);
-                console.log(chips);
+    let allChips;
+
+    function updateChips() {
+        let chips = M.Chips.getInstance($('.chips')).chipsData.map((x) => x.tag);
+        $.ajax({
+            url: "http://localhost:3003/users/" + localStorage.getItem('user') + "/keywords",
+            type: 'PUT',
+            data: JSON.stringify(chips),
+            contentType: 'application/json',
+            success: function (result) {
+                // Do something with the result
             }
         });
-    });
+    }
 
-    $('.chips').chips();
+    document.addEventListener('DOMContentLoaded', function () {
+
+        $.get("http://localhost:3003/users/" + localStorage.getItem('user'), function (data) {
+            let keywords = data.map((x) => { return {tag: x}});
+            var elems = document.querySelectorAll('.chips');
+            allChips = M.Chips.init(elems, {
+                onChipSelect: (e, chip) => {
+                    console.log(chip.childNodes[0].textContent)
+                },
+                onChipAdd: (e, chip) => {
+                    updateChips();
+                },
+                onChipDelete: (e, chip) => {
+                    updateChips()
+                },
+                data: keywords
+            });
+        });
+
+
+
+    });
 
     $('.chips-placeholder').chips({
         placeholder: 'Paraules',
