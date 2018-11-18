7c8c32a96ea5d1ebd2c1e4e9188468c62828aabd
Gigi <giselaruzafamado@gmail.com>
2018-11-18 10:15:46 +0100

    change modify

--- API/app.js
+++ API/app.js
@@ -49,7 +49,13 @@ router.get('/users/:user_email', function (req, res) {
 
 router.put('/proposals/:proposal_id', function (req, res) {
     const proposal_id = req.params['proposal_id'];
-    Database.modifyProposal(proposal_id, req.body, (proposal)=> res.json(proposal), ()=>res.status(409).send());
+    const user= req.header('user')
+    Database.modifyProposal(proposal_id, req.body,user, (proposal)=> res.json(proposal), ()=>res.status(409).send());
+});
+
+router.put('/users/:user_email', function (req, res) {
+    const user_email = req.params['user_email'];
+    Database.modifyUser(user_email,(user)=> res.json(user), ()=>res.status(409).send());
 });
 
 router.get('/proposals_historic/:proposal_id', function (req, res) {
